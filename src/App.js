e82ac45707056c86041a2351821334c289e49f00
Sachin1138 <99975080+Sachin1138@users.noreply.github.com>
2022-05-30 19:12:17 +0530

    reactEvalution-2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "@chakra-ui/react";
+import Products from "./components/Products";
+
+const App = () => {
+  return (
+    <div>
+      {/* READ: what is container in chakra-ui */}
+      <Container maxW="container.xl" textAlign="center">
+        <Products />
+        {/* Products // show by default*/}
+      </Container>
+    </div>
+  );
+};
+
+export default App;
